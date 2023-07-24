cd6af5725f75eb935a6d05b8dc0eb042180340a6
Veronchi <zhigunova1995@gmail.com>
2023-07-24 12:53:34 +0300

    customize MUI theme

--- cv/src/main.tsx
+++ cv/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './styles/theme';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>
 );
