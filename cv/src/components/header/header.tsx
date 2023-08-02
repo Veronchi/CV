1234f47c5b197b414b2ea2d7682cd2b5f0f6f88b
Veronchi <zhigunova1995@gmail.com>
2023-08-02 16:20:43 +0300

    add CV pdf

--- cv/src/components/header/header.tsx
+++ cv/src/components/header/header.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react';
 import { DayNight, NavTabs } from '..';
+import pdf from '@/assets/img/Veranika-Miadvedzeva-CV.pdf';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import './header.scss';
 
 export const Header: FC = () => {
   return (
     <header className="header">
       <NavTabs />
-      <DayNight />
+
+      <div className="header__btns">
+        <a href={pdf} className="pdf" target="_blank" rel="noopener noreferrer">
+          <PictureAsPdfIcon />
+        </a>
+        <DayNight />
+      </div>
     </header>
   );
 };
